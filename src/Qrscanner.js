2ea469bf27129c0d8ec91146256caace2c219857
lebed2045 <lebed2045@gmail.com>
2019-02-16 20:18:00 -0700

    parsed address

--- src/Qrscanner.js
+++ src/Qrscanner.js
@@ -11,33 +11,55 @@ class Qrscanner extends Component {
     this.handleScan = this.handleScan.bind(this);
   }
   handleScan(data) {
-    if (data === this.state.result) {
+    if (data && data !== this.state.result) {
       this.setState({
         result: data
       });
       //update, woohooo!
+      console.log("new qr code found!");
       // that means that we scanned some sort of qr code
       // now let's check where it's valid xDai address, and if so
       // send this amazing news to the server API and redirect to 
       // the payment page
-      const xDaiAddress = "";
-      const xDaiValue = "";
+      let xDaiAddress = "";
+      let xDaiValue = "";
       // is it buffdai address?
       const preffix = "https://buffidai.io/";
       if (data.substring(0, preffix.length) === preffix) {
-        for (let i = preffix.length; i < data.length; i++) {
-            if ( !/[^a-zA-Z0-9]/.test(data[i]) ) break;
+        console.log("found buffidai preffix!");
+        let i = 0;
+        for (i = preffix.length; i < data.length; i++) {
+            if ( /[^a-zA-Z0-9]/.test(data[i]) ) break;
             xDaiAddress += data[i];
+        }
+        //console.log("xDAi = " + xDaiAddress);
+        for ( i++; i < data.length; i++) {
+            if ( /[^0-9]/.test(data[i]) ) break;
+            xDaiValue += data[i];
         }
 
       } else {
         // is it ethereum address?
-
+        if (this.isAddress(data)) {
+            xDaiAddress = data;
+        }
       }
-      // let's call API and update the right JSON on redis
+      if (xDaiAddress) {
+        // let's call API and update the right JSON on redis
+        console.log("xDai = " + xDaiAddress + ", value = " + xDaiValue);
+      }
+      
 
     }
   }
+
+   isAddress = function (address) {
+        if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+            // check if it has the basic requirements of an address
+            return false;
+        } 
+        return true;
+    };
 
   handleError(err) {
     console.error(err);
